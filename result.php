1ae58dce0a7c583e0eb6c05549a53acbd5118185
Anh Tuan Pham <anhpham7891@gmail.com>
2012-08-27 17:31:26 +1000

    add part to handle session functionalities

--- result.php
+++ result.php
@@ -4,6 +4,8 @@ require_once('db.php');
 require_once('connectPDO.php');
 require_once('class.WineSearchController.php');
 require_once('sessionHandler.php');
+require_once('twitter.php');
+
 $smarty->assign('title','resultView');
 $smarty->assign('current_view','result');
 $smarty->assign('style_sheet_link','connect-style.css');
@@ -24,6 +26,16 @@ if($_GET['submit']=='search')
 		$smarty->assign('result_headers',array('Sorry bro,'));
 		$smarty->assign('results',array(array('Couldn\'t found the thing you were searching for! Better check yo input or look elsewhere!')));
 	}
+}
+else if($_GET['submit']=='history')
+{
+	$smarty->assign('current_view','history');
+	$smarty->assign('result_headers',array('Wine(s) found in this session:'));
+		$smarty->assign('results',$_SESSION['history']);
+}
+else if($_GET['submit']=='share')
+{
+	shareWineList($_SESSION['history']);	
 }
 else if($_GET['submit']=='')
 {
